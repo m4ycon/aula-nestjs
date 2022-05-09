aa9d92b36f9b5fa02ac46ee9f4afe3ba25f2f739
Maycon Fabio <m.vinnicyus@gmail.com>
2022-05-09 19:46:49 -0300

    tests: Add e2e tests and tests environment

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -2,4 +2,11 @@ import { PrismaClient } from '@prisma/client';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PrismaService extends PrismaClient {}
+export class PrismaService extends PrismaClient {
+  async cleanDb() {
+    await this.user.deleteMany();
+    await this.post.deleteMany();
+    await this.category.deleteMany();
+    await this.profile.deleteMany();
+  }
+}
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,18 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { PrismaService } from '../src/prisma/prisma.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
     await app.init();
+
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 
   it('/ (GET)', () => {
@@ -20,5 +34,65 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  describe('User', () => {
+    it('should create a user', () => {
+      return request(app.getHttpServer())
+        .post('/users')
+        .send({ email: 'ndp@the.best', password: '123456' })
+        .expect(201);
+    });
+
+    it('should find a user', async () => {
+      const userDto = { email: 'ndp@the.crazy', password: '123456' };
+
+      const user = await request(app.getHttpServer())
+        .post('/users')
+        .send(userDto)
+        .expect(201)
+        .then((res) => res.body);
+
+      const res = await request(app.getHttpServer())
+        .get(`/users/${user.id}`)
+        .expect(200);
+
+      expect(res.body).toEqual({
+        id: expect.any(Number),
+        name: null,
+        email: userDto.email,
+        Post: expect.arrayContaining([]),
+        Profile: expect.objectContaining({
+          id: expect.any(Number),
+          userId: expect.any(Number),
+        }),
+      });
+    });
+  });
+
+  describe('Auth', () => {
+    it('should sign up', async () => {
+      const res = await request(app.getHttpServer())
+        .post('/auth/signup')
+        .send({ email: 'ndp@the.biggest', password: '123456' })
+        .expect(201);
+
+      expect(res.body).toEqual({ access_token: expect.any(String) });
+    });
+
+    it('should sign in', async () => {
+      const user = { email: 'ndp@the.prettiest', password: '123456' };
+
+      await request(app.getHttpServer())
+        .post('/auth/signup')
+        .send(user)
+        .expect(201);
+
+      const res = await request(app.getHttpServer())
+        .post('/auth/signin')
+        .send(user);
+
+      expect(res.body).toEqual({ access_token: expect.any(String) });
+    });
   });
 });
